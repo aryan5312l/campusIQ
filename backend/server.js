d6031950e7a89152700259766da06b249d00390a
aryan5312l <aryan10000raj@gmail.com>
2026-05-07 15:30:28 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //Middleware to parse JSON body
-app.use(cors());
+app.use(cors({
+    origin: "https://nieiq.onrender.com",
+    credentials: true
+}));
 app.use(express.json());
 
 
